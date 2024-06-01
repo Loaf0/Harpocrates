cc9046fe3c817fa6a5c7e07779155bccd09e4b7f
Twig :3 <tylersmaldonado@gmail.com>
2024-06-01 01:50:30 -0400

    Programmatically update html

--- preload.js
+++ preload.js
@@ -1,10 +1,33 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
+    const contacts = ["Joe", "John", "Tyler", "Zach", "Nick", "Teddy", "Ethan", "Anneliese", "Twig"]
+    const contactsDiv = document.getElementById("contacts-list")
+
+    for (let i = 0; i < contacts.length; i++) {
+        const li = document.createElement("li")
+        li.innerText = contacts[i]
+        li.onclick = () => {
+            //TODO: switch screen to contact, load contact messages
+        }
+        contactsDiv.appendChild(li)
     }
-  
-    for (const type of ['chrome', 'node', 'electron']) {
-        replaceText(`${type}-version`, process.versions[type])
+
+    document.getElementById("add-contact").onclick = () => {
+        //TODO: display a screen to enter contact address
     }
+
+    const messagesDiv = document.getElementById("messages")
+    const createMessage = (contact, content) => {
+        const div = document.createElement("div")
+        div.className = "message"
+
+        const p = document.createElement("p")
+        p.innerHTML = `<strong>${contact}:</strong> ${content}`
+        div.appendChild(p)
+        messagesDiv.appendChild(div)
+    }
+
+    //? example
+    createMessage("Contact 1", "Hello!")
+    createMessage("You", "Hi, how are you?")
+    createMessage("Contact 1", "I'm good, thanks!")
 })
