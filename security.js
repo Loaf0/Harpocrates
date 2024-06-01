d900e6a87a38d850d5863ac8bfdb11f8d951ce07
Twig :3 <tylersmaldonado@gmail.com>
2024-06-01 02:50:17 -0400

    Crypto template functions

---
+++ security.js
@@ -0,0 +1,19 @@
+const crypto = require('crypto')
+
+const createKeys = (password) => {
+    /** 
+     * TODO
+     * PrivateKeyEncryption(SHA256(password))
+     * Write keys to disk, PEM format
+     */
+}
+
+const encryptMessage = (publicKey, rawMessage) => {
+
+}
+
+const decryptMessage = (rawPrivateKey, encryptedMessage) => {
+
+}
+
+createKeys("supersecretpassword")
