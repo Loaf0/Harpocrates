d045c7ef95eebb10a5c190d5055e19dc5779f86e
Twig :3 <tylersmaldonado@gmail.com>
2024-06-01 00:49:18 -0400

    Added WebSocket template functions

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
+const { WebSocket, WebSocketServer } = require('ws')
+
+const wss = new WebSocketServer({ port: 37095 })
+let ws;
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
@@ -16,18 +20,52 @@ function createWindow() {
     mainWindow.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-    createWindow()
+wss.on('connection', (ws) => {
+    ws.on('error', console.error);
 
+    ws.on('message', (data) => {
+
+    })
+
+    // send current user contact info
+    ws.send(JSON.stringify({
+        'ipAddress': '',
+        'displayName': '',
+        'pubKey': ''
+    }))
+})
+
+const attemptConnection = (address) => {
+    ws = new WebSocket(`ws://${address}`)
+}
+
+// client timeout
+ws.on('error', console.error)
+
+// connection to server successful
+ws.on('open', () => {
+
+})
+
+// received data from server
+ws.on('message', (data) => {
+
+})
+
+app.whenReady().then(() => {
+    createWindow();
+
+    // retry to create window
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
+            createWindow();
         }
     })
 })
 
+// quit the app once window closes
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 })
