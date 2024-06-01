67f4f7a7e27bd5cc7e46cca9bc2724062382cece
Loaf0 <110581361+Loaf0@users.noreply.github.com>
2024-05-31 23:07:44 -0400

    Co-authored-by: Twig <tylersmaldonado@gmail.com>

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const { exec } = require('child_process');
-
-let serverProcess;
+const { app, BrowserWindow } = require('electron/main');
+const path = require('node:path');
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
@@ -13,35 +10,22 @@ function createWindow() {
         }
     });
 
-    mainWindow.setMenu(null)
+    mainWindow.setMenu(null);
     mainWindow.loadFile('index.html');
 }
 
-app.on('ready', () => {
-    serverProcess = exec('node server.js', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error.message}`);
-        }
-        if (stderr) {
-            console.error(`Stderr: ${stderr}`);
-        }
-        console.log(`Stdout: ${stdout}`);
-    });
+app.whenReady().then(() => {
+    createWindow()
 
-    createWindow();
-});
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit();
+        app.quit()
     }
-    if (serverProcess) {
-        serverProcess.kill();
-    }
-});
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
+})
