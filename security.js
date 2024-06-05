4cb6a5d023dc50015cce087ca50bbeda0b37fd3e
Twig :3 <tylersmaldonado@gmail.com>
2024-06-05 00:19:27 -0400

    security.js formatting

--- security.js
+++ security.js
@@ -1,96 +1,99 @@
 import * as crypto from 'crypto'
 import * as eccrypto from 'eccrypto'
 
-// utf8
+const b64 = 'base64url'
+
+/**
+ * Generate the EC key pair
+ * @param {String} password User chosen password (UTF8)
+ * @returns {Object} Key pair
+ */
 const createKeys = (password) => {
     const privateKey = eccrypto.generatePrivate();
-    const publicKey = eccrypto.getPublic(privateKey).toString('base64url');
-    const passwordHash = crypto.createHash('sha256').update(password).digest('hex');
+    const publicKey = eccrypto.getPublic(privateKey).toString(b64);
+    const passwordHash = crypto.createHash('sha256').update(password).digest();
 
-    const iv = crypto.randomBytes(16).toString('base64url')
-    const cipher = crypto.createCipheriv('aes-256-gcm', Buffer.from(passwordHash, 'hex'), Buffer.from(iv, 'base64url'));
-    let encryptedPrivateKey = cipher.update(privateKey.toString('hex'), 'hex', 'base64url');
-    encryptedPrivateKey += cipher.final('base64url')
-    const tag = cipher.getAuthTag().toString('base64url')
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv('aes-256-gcm', passwordHash, iv);
+    const encryptedPrivateKey = Buffer.concat([cipher.update(privateKey), cipher.final()]);
+    const tag = cipher.getAuthTag().toString(b64);
 
-    const encryptedPrivateKeyIV = `${encryptedPrivateKey}$$${tag}$$${iv}`
-    return { publicKey, encryptedPrivateKeyIV }
+    // format: privateKey$$authTag$$IV
+    const encryptedPrivateKeyIV = `${encryptedPrivateKey.toString(b64)}$$${tag}$$${iv.toString(b64)}`;
+    return { publicKey, encryptedPrivateKeyIV };
 }
 
 /**
- * 
- * @param {String} password 
- * @param {String} encryptedPrivateKeyIV 
- * @returns {String} base64
+ * Takes an encrypted private key and a password to decrypt the private key
+ * @param {String} password User chosen password (UTF8)
+ * @param {String} encryptedPrivateKeyIV Private key in format (base64$$base64$$base64)
+ * @returns {String} User decrypted private key (base64)
  */
 const decryptPrivateKey = (password, encryptedPrivateKeyIV) => {
-    const passwordHash = crypto.createHash('sha256').update(password).digest('hex');
-    const [encryptedPrivateKey, tag, iv] = encryptedPrivateKeyIV.split('$$')
+    const passwordHash = crypto.createHash('sha256').update(password).digest();
+    const [encryptedPrivateKey, tag, iv] = encryptedPrivateKeyIV.split('$$');
 
-    const decipher = crypto.createDecipheriv('aes-256-gcm', Buffer.from(passwordHash, 'hex'), Buffer.from(iv, 'base64url'))
-    decipher.setAuthTag(Buffer.from(tag, 'base64url'))
+    const decipher = crypto.createDecipheriv('aes-256-gcm', passwordHash, Buffer.from(iv, b64));
+    decipher.setAuthTag(Buffer.from(tag, b64));
 
-    let rawPrivateKey = decipher.update(encryptedPrivateKey, 'base64url', 'base64url')
-    rawPrivateKey += decipher.final('base64url')
-    return rawPrivateKey
+    const rawPrivateKey = Buffer.concat([decipher.update(encryptedPrivateKey, b64), decipher.final()]);
+    return rawPrivateKey.toString(b64);
 }
 
 /**
- * Encrypts an plaintext message with contacts public key
- * @param {String} publicKey Base64
- * @param {String} rawMessage UTF-8 
- * @returns {Promise<String>} Base64
- */
-const encryptMessage = async (publicKey, rawMessage) => {
-    const ecies = await eccrypto.encrypt(Buffer.from(publicKey, 'base64url'), Buffer.from(rawMessage, 'utf-8'))
-    return Buffer.from(JSON.stringify(formatECIES(ecies, 'base64url')), 'utf8').toString('base64url')
-}
-
-/**
- * TODO: too messy
- * @param {eccrypto.Ecies} rawECIES  
- * @param {('base64url'|'Buffer')} encode
+ * Format ECIES object properties to either Base64 or to a Buffer
+ * @param {eccrypto.Ecies} rawECIES ECIES JS Object
+ * @param {('base64url'|'Buffer')} encode Encoding to convert
+ * @returns {eccrypto.Ecies} Converted ECIES
  */
 const formatECIES = (rawECIES, encode) => {
-    if (encode === 'base64url') {
-        rawECIES.iv = Buffer.from(rawECIES.iv).toString(encode)
-        rawECIES.ephemPublicKey = Buffer.from(rawECIES.ephemPublicKey).toString(encode)
-        rawECIES.ciphertext = Buffer.from(rawECIES.ciphertext).toString(encode)
-        rawECIES.mac = Buffer.from(rawECIES.mac).toString(encode)
-        return rawECIES
+    for (const key in rawECIES) {
+        // Encode properties in base64
+        if (encode === b64) {
+            rawECIES[key] = Buffer.from(rawECIES[key]).toString(encode); continue;
+        }
+        // else Encode to Buffer
+        rawECIES[key] = Buffer.from(rawECIES[key], b64);
     }
-
-    rawECIES.iv = Buffer.from(rawECIES.iv, 'base64url')
-    rawECIES.ephemPublicKey = Buffer.from(rawECIES.ephemPublicKey, 'base64url')
-    rawECIES.ciphertext = Buffer.from(rawECIES.ciphertext, 'base64url')
-    rawECIES.mac = Buffer.from(rawECIES.mac, 'base64url')
-    return rawECIES
+    return rawECIES;
 }
 
 /**
- * 
- * @param {String} rawPrivateKey 
- * @param {String} encryptedMessage 
+ * Encrypts a plaintext message with chosen contact public key
+ * @param {String} publicKey Contact public key (base64)
+ * @param {String} plainText Message to encrypt (UTF-8) 
+ * @returns {Promise<String>} CipherText with prams (base64)
+ */
+const encryptMessage = async (publicKey, plainText) => {
+    const ecies = await eccrypto.encrypt(Buffer.from(publicKey, b64), Buffer.from(plainText, 'utf8'));
+    return Buffer.from(JSON.stringify(formatECIES(ecies, b64)), 'utf8').toString(b64);
+}
+
+/**
+ * Decrypts a ciphertext message with user private key
+ * @param {String} rawPrivateKey User decrypted private key (base64)
+ * @param {String} encryptedMessage Ciphertext to decrypt (UTF-8)
+ * @returns {Promise<String>} Plaintext message (UTF-8)
  */
 const decryptMessage = async (rawPrivateKey, encryptedMessage) => {
-    const ecies = formatECIES(JSON.parse(Buffer.from(encryptedMessage, 'base64url').toString('utf8')), 'Buffer')
-    return (await eccrypto.decrypt(Buffer.from(rawPrivateKey, 'base64url'), ecies)).toString('utf-8')
+    const ecies = formatECIES(JSON.parse(Buffer.from(encryptedMessage, b64).toString('utf8')), 'Buffer');
+    return (await eccrypto.decrypt(Buffer.from(rawPrivateKey, b64), ecies)).toString('utf8');
 }
 
 const test = async () => {
     const bobKeys = createKeys("secretpassword1");
     const aliceKeys = createKeys("secretpassword2");
 
-    const messageToSend = "Hello Alice!"
+    const messageToSend = "Hello Alice!";
     const bobMessage = await encryptMessage(aliceKeys.publicKey, messageToSend);
 
-    const aliceRawPrivateKey = decryptPrivateKey("secretpassword2", aliceKeys.encryptedPrivateKeyIV)
-    const messageReceived = await decryptMessage(aliceRawPrivateKey, bobMessage)
+    const aliceRawPrivateKey = decryptPrivateKey("secretpassword2", aliceKeys.encryptedPrivateKeyIV);
+    const messageReceived = await decryptMessage(aliceRawPrivateKey, bobMessage);
 
-    const didTestPass = messageToSend === messageReceived
-    console.log(`Test Passed? ${didTestPass}`)
+    const didTestPass = messageToSend === messageReceived;
+    console.log(`Test Passed? ${didTestPass}`);
 }
 
-test()
+test();
 
 
